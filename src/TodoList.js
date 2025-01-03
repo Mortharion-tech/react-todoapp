4f711fb43e0f6c905f5edebe746e5588ef10c09f
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-03 20:59:19 +0100

    Moving code to own component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,27 +1,15 @@
+import TodoItem from './TodoItem';
+
 export default function TodoList({ tasks, dispatch }) {
     return (
-        <>
-        {tasks.map(task =>
-        <li key={task.id}>
-            <input
-                onChange={() =>
-                    dispatch({
-                        type: 'UPDATE_TODO',
-                        task: {...task, done: !task.done }
-                    })
-                }
-                type="checkbox" checked={task.done}
-            />
-            {task.text}
-            <button onClick={() => 
-                dispatch({
-                    type: 'DELETE_TODO',
-                    taskId: task.id
-                })}>
-                        Delete
-                </button>
-        </li>
-        )}
-        </>
-    )
+        <ul>
+            {tasks.map(task =>
+                <TodoItem
+                    key={task.id}
+                    task={task}
+                    dispatch={dispatch}
+                />
+            )}
+        </ul>
+    );
 }
